e2279fee146ba52388635499be6f2648ce34947b
Valentin Gikhman <valentin.gikhman@gmail.com>
2026-01-06 17:36:06 -0500

    Fixed typo, add canonical url route

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace GTX {
             );
 
             routes.MapRoute(
+                name: "VehicleSeo",
+                url: "inventory/{year}-{name}-{stock}",
+                defaults: new { controller = "Inventory", action = "Details" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
