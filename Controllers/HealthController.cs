f7097cf896c45deb8efdfa72afbdc361d66d2660
Valentin Gikhman <valentin.gikhman@gmail.com>
2026-01-19 21:34:02 -0500

    Added health page

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,53 @@
+﻿using GTX.Models;
+using System;
+using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace GTX.Controllers
+{
+    public class HealthController : Controller
+    {
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            var p = Process.GetCurrentProcess();
+
+            long procWorkingSet = p.WorkingSet64;
+            long procPrivateBytes = p.PrivateMemorySize64;
+            long procPaged = p.PagedMemorySize64;
+
+            long? committedBytes = null;
+            long? availBytes = null;
+
+            try
+            {
+                using (var committedCounter = new PerformanceCounter("Memory", "Committed Bytes"))
+                using (var availCounter = new PerformanceCounter("Memory", "Available Bytes"))
+                {
+                    availBytes = Convert.ToInt64(availCounter.NextValue());
+                    committedBytes = Convert.ToInt64(committedCounter.NextValue());
+                }
+            }
+            catch { /* ignore */ }
+
+            long managed = GC.GetTotalMemory(false);
+
+            var model = new HealthModel
+            {
+                ServerTime = DateTimeOffset.Now,
+                ProcessName = p.ProcessName,
+                Pid = p.Id,
+                WorkingSetBytes = procWorkingSet,
+                PrivateBytes = procPrivateBytes,
+                PagedBytes = procPaged,
+                ManagedBytes = managed,
+                AvailableBytes = availBytes,
+                CommittedBytes = committedBytes
+            };
+
+            return View("Index", model);
+        }
+    }
+}
---
+++ Models/HealthModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GTX.Models
+{
+    public class HealthModel: BaseModel
+    {
+        public DateTimeOffset ServerTime { get; set; }
+
+        // process
+        public string ProcessName { get; set; }
+        public int Pid { get; set; }
+        public long WorkingSetBytes { get; set; }
+        public long PrivateBytes { get; set; }
+        public long PagedBytes { get; set; }
+        public long ManagedBytes { get; set; }
+
+        // machine
+        public long? AvailableBytes { get; set; }
+        public long? CommittedBytes { get; set; } // committed virtual memory
+
+    }
+}
